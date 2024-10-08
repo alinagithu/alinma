4a9b84a7e703b6b95d8745dafaa9085120afff2d
alinagithu <alinmaex3@gmail.com>
2024-10-08 19:04:12 +0300

    Add files via upload

---
+++ 20241008153810-RBFKTSHS.js
@@ -0,0 +1,37 @@
+window.botpress.init({
+  "botId": "6205fd9a-131b-410f-a793-a2499a3f988e",
+  "configuration": {
+    "composerPlaceholder": "اكتب رسالتك هنا",
+    "botName": "شركة الإنماء إكسبرس",
+    "botAvatar": "https://files.bpcontent.cloud/2024/10/08/15/20241008153925-91CSNJKR.jpeg",
+    "botDescription": "المساعد الذكي",
+    "website": {
+      "title": "https://www.alinmaex.com",
+      "link": "https://www.alinmaex.com"
+    },
+    "email": {
+      "title": "info@alinmaex.com",
+      "link": "info@alinmaex.com"
+    },
+    "phone": {
+      "title": "+967 234 4041",
+      "link": "+967 234 4041"
+    },
+    "termsOfService": {
+      "title": "https://www.facebook.com/alinmaex",
+      "link": "https://www.facebook.com/alinmaex"
+    },
+    "privacyPolicy": {
+      "title": "https://whatsapp.com/channel/0029Vaiql3B1Hsq4lRKfR40W",
+      "link": "https://whatsapp.com/channel/0029Vaiql3B1Hsq4lRKfR40W"
+    },
+    "color": "#3B82F6",
+    "variant": "solid",
+    "themeMode": "light",
+    "fontFamily": "inter",
+    "radius": 1,
+    "showPoweredBy": false,
+    "additionalStylesheet": ""
+  },
+  "clientId": "a17cec04-2246-4beb-a22b-e896185e7c08"
+});
